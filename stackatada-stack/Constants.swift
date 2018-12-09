14ada84004eab9bffe5ade8619caec8e5ddf88ce
Dilyan Ergeledzhiev <dergeledji@gmail.com>
2018-12-09 23:37:49 +0100

    working with uistackview

---
+++ stackatada-stack/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  stackatada-stack
+//
+//  Created by sade on 20/4/17.
+//  Copyright © 2017 sade. All rights reserved.
+//
+
+import UIKit
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ stackatada-stack/FancyBtn.swift
@@ -0,0 +1,24 @@
+//
+//  FancyBtn.swift
+//  stackatada-stack
+//
+//  Created by sade on 20/4/17.
+//  Copyright © 2017 sade. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 5.0
+        
+    }
+
+}
